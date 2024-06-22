0e20b0ebafa06a6adb8966b61fe772f48a7005ca
Wahab Khan <167424483+wahabkhaan@users.noreply.github.com>
2024-06-22 06:30:55 +0500

    Create Game.sh

---
+++ Game.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# A simple script to run the Python game
+echo -e "\033[95m##############################################"
+echo -e "#                                            #"
+echo -e "#         Welcome to Guess the Number!       #"
+echo -e "#                                            #"
+echo -e "##############################################\033[0m"
+
+python3 guess_the_number.py
